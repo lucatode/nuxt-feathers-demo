febfe4a2b399bfa4c5bffe59d2d61b4611a414ec
woodgates <jorge.yoma@outlook.com>
2018-09-09 18:59:28 -0300

    Update index.js
    services are loaded in the middleware.

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const nuxt = require('./nuxt')
+const services = require('../services');
 
 module.exports = function () {
   // Add your custom middleware here. Remember, that
   // just like Express the order matters, so error
   // handling middleware should go last.
+  this.configure(services);
+  
   const app = this
 
   app.use(nuxt)
